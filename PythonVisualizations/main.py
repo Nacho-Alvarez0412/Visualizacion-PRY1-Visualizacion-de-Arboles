1121c6b58e65124e3fceca3609cdff9ff04c37fd
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-09-03 18:26:12 -0600

    implemented dropdown updates for icicle plot

--- PythonVisualizations/main.py
+++ PythonVisualizations/main.py
@@ -2,6 +2,7 @@ import dash
 import pandas as pd
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 from visualizations.plots import create_icicle_plot
 
 
@@ -53,6 +54,18 @@ app.layout = html.Div([
   # Footer
   html.Footer(className="main-footer", children="Desarrollado por Ignacio Álvarez y Andrés Aguilar")
 ])
+
+@app.callback(
+    Output("hs2-dropdown", "options"),
+    Input("section-dropdown", "value")
+)
+def on_section_selected(selected_section):
+    # Get hs2 groups by section name
+    # TODO: Implement updates for icicle plot
+    hs2_groups = country_exports_data[country_exports_data["Section"] == selected_section]["HS2"].unique()
+    return [{"label": i, "value": i}  for i in list(hs2_groups)]
+
+
 
 if __name__ == "__main__":
   app.run_server(debug=True, port=4000, use_reloader=True)
