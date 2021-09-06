80e7f24bc3a34aaf70177fb79e42a41fa6cf8ea7
aguilarandress <andres.aguilarmoya2001@gmail.com>
2021-09-06 13:43:57 -0600

    added filtering to sunburst chart

--- PythonVisualizations/view/callbacks.py
+++ PythonVisualizations/view/callbacks.py
@@ -1,10 +1,38 @@
 from dash.dependencies import Input, Output
-from visualizations.charts import create_icicle_chart
+from visualizations.charts import create_icicle_chart, create_sunburst_chart
 
 
 def register_callbacks(app, exportations_data):
+    @app.callback(
+        Output("sunburst-hs2-dropdown", "options"),
+        Output("sunburst-chart", "figure"),
+        Input("sunburst-section-dropdown", "value"),
+        Input("sunburst-hs2-dropdown", "value")
+    )
     def filter_sunburst_products(selected_section, selected_hs2):
-        pass
+        if (not selected_section and not selected_hs2) or (not selected_section and selected_hs2):
+            return [], create_sunburst_chart(exportations_data)
+        elif selected_section and not selected_hs2:
+            section_products = exportations_data[exportations_data["Section"] == selected_section]
+            # Get hs2 groups by section name
+            hs2_groups = section_products["HS2"].unique()
+            hs2_dropdown_options = [{"label": i, "value": i} for i in list(hs2_groups)]
+            section_icicle_plot = create_sunburst_chart(section_products)
+            return hs2_dropdown_options, section_icicle_plot
+        elif selected_section and selected_hs2:
+            hs2_groups = exportations_data[exportations_data["Section"] == selected_section]["HS2"].unique()
+            hs2_dropdown_options = [{"label": i, "value": i} for i in list(hs2_groups)]
+            filtered_products = exportations_data[
+                (exportations_data["Section"] == selected_section)
+                & (exportations_data["HS2"] == selected_hs2)]
+            # No products with section and hs2
+            if filtered_products.empty:
+                section_products = exportations_data[exportations_data["Section"] == selected_section]
+                section_icicle_plot = create_sunburst_chart(section_products)
+                return hs2_dropdown_options, section_icicle_plot
+
+            hs2_icicle_plot = create_sunburst_chart(filtered_products)
+            return hs2_dropdown_options, hs2_icicle_plot
 
 
     @app.callback(
